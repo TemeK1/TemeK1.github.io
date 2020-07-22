66ccc35780fc03fd0164af2d5e7735c31edd167c
TemeK1 <50360933+TemeK1@users.noreply.github.com>
2020-07-22 19:20:16 +0300

    Add files via upload

---
+++ notes/precache-manifest.03ec02342c6168c296d13261139df7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c804355de1ffbe4e7fb9ae39385743",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "f33941318bbd8fe96fde",
+    "url": "/notes/static/css/main.6a9372bf.chunk.css"
+  },
+  {
+    "revision": "7dbfab0d7eb3bf0f4dd7",
+    "url": "/notes/static/js/2.dbf466aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notes/static/js/2.dbf466aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33941318bbd8fe96fde",
+    "url": "/notes/static/js/main.b3e22804.chunk.js"
+  },
+  {
+    "revision": "b4e7f333cec4847613e3",
+    "url": "/notes/static/js/runtime-main.9f0c36eb.js"
+  }
+]);
