22826e3c5912478facd10440540c24f8252f384a
Teemu Käpylä <teemukapyla@gmail.com>
2020-08-04 11:43:52 +0300

    add species

---
+++ species/precache-manifest.0c1aabbcd1061501fcbef45b56c9e1d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4021cfb6634abde940692f5ce41b15",
+    "url": "/species/index.html"
+  },
+  {
+    "revision": "30376bf4368bf43ca591",
+    "url": "/species/static/css/main.fc89d124.chunk.css"
+  },
+  {
+    "revision": "b75303be44a70553462a",
+    "url": "/species/static/js/2.e677c59e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/species/static/js/2.e677c59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30376bf4368bf43ca591",
+    "url": "/species/static/js/main.832d872d.chunk.js"
+  },
+  {
+    "revision": "0f18fdfce5e8802b70ba",
+    "url": "/species/static/js/runtime-main.c44f2aed.js"
+  }
+]);
