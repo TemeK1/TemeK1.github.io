320aa0677837246ed3a6d52cd41866e1601a145a
Teemu Käpylä <teemukapyla@gmail.com>
2020-09-10 00:16:38 +0300

    update

--- scroll.js
+++ scroll.js
@@ -19,6 +19,12 @@
         }
       }
     });
+    
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 80
+      });
   
     // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function() {
